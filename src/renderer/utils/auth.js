b76542cbed7838c588c865c0cca68ef56e324594
windyao <yao_wind79@hotmail.com>
2021-02-10 00:05:38 +0800

    编包后,js无法操作cookie,将影响登录的地方由cookie改为localstorage

--- src/renderer/utils/auth.js
+++ src/renderer/utils/auth.js
@@ -1,15 +1,18 @@
-import Cookies from 'js-cookie'
+// import Cookies from 'js-cookie'
 
 const TokenKey = 'Admin-Token'
 
 export function getToken() {
-  return Cookies.get(TokenKey)
+  // return Cookies.get(TokenKey)
+  return localStorage.getItem(TokenKey) || ''
 }
 
 export function setToken(token) {
-  return Cookies.set(TokenKey, token)
+  // return Cookies.set(TokenKey, token)
+  localStorage.setItem(TokenKey, token)
 }
 
 export function removeToken() {
-  return Cookies.remove(TokenKey)
+  // return Cookies.remove(TokenKey)
+  localStorage.removeItem(TokenKey)
 }
