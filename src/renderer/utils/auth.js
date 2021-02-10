fdf1cebcda4003d4297ee49d1414a36f01d380a2
windyao <yao_wind79@hotmail.com>
2021-02-10 22:54:00 +0800

    使用electron-cookies, 支持document.cookie

--- src/renderer/utils/auth.js
+++ src/renderer/utils/auth.js
@@ -1,18 +1,15 @@
-// import Cookies from 'js-cookie'
+import Cookies from 'js-cookie'
 
 const TokenKey = 'Admin-Token'
 
 export function getToken() {
-  // return Cookies.get(TokenKey)
-  return localStorage.getItem(TokenKey) || ''
+  return Cookies.get(TokenKey)
 }
 
 export function setToken(token) {
-  // return Cookies.set(TokenKey, token)
-  localStorage.setItem(TokenKey, token)
+  return Cookies.set(TokenKey, token)
 }
 
 export function removeToken() {
-  // return Cookies.remove(TokenKey)
-  localStorage.removeItem(TokenKey)
+  return Cookies.remove(TokenKey)
 }
